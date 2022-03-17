60a7393d23f55a7ccbe7a074ae1a9a56173f9e9a
xniola <stefano.perniola0@gmail.com>
2022-03-17 15:50:23 +0100

    daje

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,22 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionPianoStudiInformatica(Action):
+
+    def name(self) -> Text:
+        return "action_piano_studi_informatica"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text=  "Ecco il piano di studi LM per Ingegneria Informatica e dell'Automazione!")
+        dispatcher.utter_message(image= 'https://www.ingegneria.univpm.it/sites/www.ingegneria.univpm.it/files/ingegneria/guida_ingegneria/guida_2021_2022/pdf_270/IM12_LM_IA_20.pdf')
+        
+        return []
